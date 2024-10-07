987bc5c8ad54db6421a6db3eb8908394fc1a27f8
Parker Jazayeri <parkerjazayeri@gmail.com>
2024-10-07 16:31:24 -0700

    add color menu balloons on hover

--- squibble/src/ColorMenu.js
+++ squibble/src/ColorMenu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "balloon-css";
 
 const ColorMenu = ({ currentColor, handleSampleColorSelect, handleCustomColorSelect, sampleColors }) => {
   return (
@@ -8,6 +9,8 @@ const ColorMenu = ({ currentColor, handleSampleColorSelect, handleCustomColorSel
           key={index}
           className="color-sample"
           style={{ backgroundColor: color }}
+          aria-label= {"Color ".concat((index+1))}
+          data-balloon-pos="left"
           onClick={(e) => {
             e.stopPropagation(); // Prevent deselecting the tool
             handleSampleColorSelect(color, index);
@@ -17,13 +20,14 @@ const ColorMenu = ({ currentColor, handleSampleColorSelect, handleCustomColorSel
     {/* Custom Color Picker with Rainbow Border */}
     <div className="custom-color-container">
       <div className="rainbow-border"></div>
+      <div className="balloon-wrapper" aria-label= "Custom Color" data-balloon-pos="left">
         <input
           type="color"
           value={currentColor}
           onChange={handleCustomColorSelect}
           className="custom-color-picker"
-          title="Choose Custom Color"
         />
+      </div>
       </div>
     </div>
   );
